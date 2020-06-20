70ccf466f1c20f06a682e152e153c730856b39f9
sohamkornchain <67193204+sohamkornchain@users.noreply.github.com>
2020-06-20 19:16:49 +0530

    Keys annonymized.

--- config/default.js
+++ config/default.js
@@ -1,12 +1,12 @@
 module.exports = {
   "auth": {
     "URL": "https://auth-developer-dev.myloyalt.com/oauth2/token",
-    "ClientId": "27b2ap8pnlj36hhh32dcdmglo0",
-    "ClientSecret": "9gvk87q5unfq81st8vjl2inbpa8gim32gtkipaqd4hip81juvv0"
+    "ClientId": "<Your client id>", //Refer LaaS portal Developer menu
+    "ClientSecret": "<Your client secret>", //Refer LaaS portal Developer menu
   },
   "api": {
     "URL": "https://api-dev.myloyalt.com",
     "version": "v1",
-    "apiKey": "DZyPqs6MOZ78ezuQ8zDr1tnbjG74hrB1uwU9Zp8b"
+    "apiKey": "Your api key" //Refer LaaS portal Developer menu
   }
 };
