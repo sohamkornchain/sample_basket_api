e4efd5eee4192f48f3597c66d9a3d11585c4f07f
sohamkornchain <67193204+sohamkornchain@users.noreply.github.com>
2020-06-20 19:10:57 +0530

    config added

---
+++ config/default.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "auth": {
+    "URL": "https://auth-developer-dev.myloyalt.com/oauth2/token",
+    "ClientId": "27b2ap8pnlj36hhh32dcdmglo0",
+    "ClientSecret": "9gvk87q5unfq81st8vjl2inbpa8gim32gtkipaqd4hip81juvv0"
+  },
+  "api": {
+    "URL": "https://api-dev.myloyalt.com",
+    "version": "v1",
+    "apiKey": "DZyPqs6MOZ78ezuQ8zDr1tnbjG74hrB1uwU9Zp8b"
+  }
+};
